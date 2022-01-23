0efbc0b0e931ac6156924959614e8bf6e2caeccd
arpa12 <arpa15-12567@diu.edu.bd>
2022-01-23 21:28:36 +0600

    first commit

---
+++ hello.js
@@ -0,0 +1,27 @@
+var bananaPrice = 12;
+console.log(bananaPrice);
+
+var marketPrice = 3000;
+var computerPrice = 37000;
+var penPrice = 50;
+console.log(marketPrice);
+console.log(typeof computerPrice);
+console.log(penPrice);
+
+var seenAfter = 21;
+console.log(typeof seenAfter);
+
+var name = "kolimuddin  zuckerburg";
+console.log(typeof name);
+
+var isHot = true;
+var isRich = false;
+console.log(typeof isHot);
+
+var sistersAge = 10;
+var ceoName = "mark zukerburg";
+var willgetthejobinGoogle = true;
+var notgettheJob = false;
+console.log(typeof sistersAge);
+console.log(typeof ceoName);
+console.log(typeof willgetthejobinGoogle);
