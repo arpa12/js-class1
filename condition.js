33fab74060fdb4356787d6a2b24fe77e32c0ffd2
arpa12 <arpa15-12567@diu.edu.bd>
2022-01-24 01:06:23 +0600

    added 11th class

--- condition.js
+++ condition.js
@@ -1,7 +1,19 @@
-var biscuitPrice = 12;
-if (biscuitPrice == 10) {
+var biscuitPrice = 10;
+if (biscuitPrice != 10) {
     console.log("I will eat the biscuit");
 
 } else {
     console.log("kichui khabo na");
+}
+
+//..11th class..//
+var job = true;
+var savingAmount = 5000;
+if (job == true || savingAmount > 200000) {
+    console.log("marrige proposal done");
+
+} else if (job = true) {
+    console.log("after some time");
+} else {
+    console.log("dont get marry");
 }
